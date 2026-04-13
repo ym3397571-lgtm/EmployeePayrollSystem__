1f7cce8f62f4ff18c81faced30f2b59c239d11c8
Eyad-waleed <58422391+Eyad-waleed@users.noreply.github.com>
2026-04-13 15:20:46 +0200

    Fixed file paths and added gitignore for team collaboration

---
+++ EmployeePayrollSystem.cpp
@@ -0,0 +1,22 @@
+#include "data.h"
+#include "admin.h"
+#include "employee.h"
+
+#include <iostream>
+using namespace std;
+
+// =======================
+// main.cpp
+// =======================
+
+int main() {
+    cout << "Welcome to Employee Payroll Management System" << endl;
+    cout << "HI" << endl;
+
+    // === main menu ===
+    // 1. Admin Login
+    // 2. Employee Login
+    // 3. Exit
+
+    return 0;
+}
---
+++ admin.cpp
@@ -0,0 +1,13 @@
+﻿#include "data.h"
+#include "admin.h"ٍ
+#include <iostream>
+using namespace std;
+
+void calculateSalary()
+{
+    double TotalHoursWorked = 0;
+    double BonusHours = TotalHoursWorked - WorkHoursPerMonth;
+    double BonusSalary = (BasicSalary / WorkHoursPerMonth) * BonusHours;
+    double Tax = (BasicSalary * TaxRate);
+    double NetSalary = (BasicSalary + BonusSalary) - Tax;
+}
